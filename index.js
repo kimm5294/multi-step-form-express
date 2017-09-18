a982982e51d6050773aab85cfec951d9a8be3dbe
Matt Kim <kimm5294@gmail.com>
2017-09-18 00:25:14 -0400

    routing

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/profiles", (req,res)=>{
   let profiles = Profile.find();
-  res.render("test.ejs");
+  res.render("./public/test.ejs");
 })
 
 app.post("/profiles", (req,res)=>{
